256e174a46002617a43bbf7ed4026834e84db565
Anubhab Chakraborty <anubhab18@iiserb.ac.in>
2021-11-27 13:04:19 +0530

    crated pyamiimage.py and pipeline.py

---
+++ pyimage/pipeline.py
@@ -0,0 +1,9 @@
+"""
+This file sets the pipeline of pyamiimage
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ pyimage/pyamiimage.py
@@ -0,0 +1,22 @@
+"""
+This file handles the cli of pyamiimage
+Parses the commandline arguments and executes the pyamiimage pipeline
+"""
+
+
+class Pyamiimage:
+    """[summary]"""
+
+    def __init__(self) -> None:
+        pass
+
+    def handlecli(self):
+        """Handles the command line interface using argparse"""
+        pass
+
+def main():
+    runpyamiiamge = Pyamiimage()
+    runpyamiiamge.handlecli()
+
+if __name__ == '__main__':
+    main()
